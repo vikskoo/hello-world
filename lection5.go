ce798b161359a5a0c061ef0480c21248d277211b
vikskoo <webkoda@mail.ru>
2022-04-05 15:28:16 +0500

    lection5 complete

--- lection5.go
+++ lection5.go
@@ -24,4 +24,28 @@ func main() {
 	} else {
 		fmt.Println("Unknown color")
 	}
+
+	//Good. Инициализация в блоке условного оператора
+	//Блок присваивания - только :=
+	//Инициализированная переменная видна только внутри области видимости условного оператора
+	if num := 10; num%2 == 0 {
+		fmt.Println("EVEN")
+	} else {
+		fmt.Println("ODD")
+	}
+
+	//Не идеоматично
+	if width := 100; width > 100 {
+		fmt.Println("Width > 100")
+	} else {
+		fmt.Println("width <= 100")
+	}
+
+	//Странное правило номер 1: в Go стараются избегать блоков Else
+	// Идеоматично!
+	if height := 100; height > 100 {
+		fmt.Println("height > 100")
+		return
+	}
+	fmt.Println("Height <= 100")
 }
